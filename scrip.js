ee159b28cca5e097492e77a94653229883d7741c
Caio Fernandes <133905605+caiolf9616@users.noreply.github.com>
2024-07-10 18:52:41 -0300

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,8 +1,8 @@
-let numero = parseInt(prompt("Insira um número inteiro positivo:"));
+let numero = Number(prompt("Insira um número inteiro positivo:"))
 
 
 if (isNaN(numero) || numero < 0) {
-    console.log("Por favor, insira um número inteiro positivo.");
+    console.log("Por favor, insira um número inteiro positivo.")
 } else {
     let fatorial = 1;
     for (let i = 1; i <= numero; i++) {
@@ -12,14 +12,14 @@ if (isNaN(numero) || numero < 0) {
     
     let fibonacci = [0, 1];
     for (let i = 2; i <= numero; i++) {
-        fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+        fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2]
     }
 
     
     if (numero == 0) {
         fibonacci = [0];
     } else if (numero == 1) {
-        fibonacci = [0, 1];
+        fibonacci = [0, 1]
     } else {
         fibonacci = fibonacci.slice(0, numero + 1)
     }
